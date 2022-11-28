204cc36d57a68049b923fe91f1e4054789aa7267
TheRobert0728 <69301840+TheRobert0728@users.noreply.github.com>
2022-11-28 16:19:55 +0800

    Add files via upload

--- src/main/java/hk/edu/polyu/comp4133/index/PostingList.java
+++ src/main/java/hk/edu/polyu/comp4133/index/PostingList.java
@@ -1,6 +1,9 @@
+
 package hk.edu.polyu.comp4133.index;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * A posting list is a list @see{Posting}s _ordered_ by the document id. It is also iterable (over the underlying postings).
@@ -9,16 +12,18 @@ public class PostingList implements Iterable<Posting> {
     /**
      * Add a posting to the posting list
      */
+    List<Posting> postingList = new ArrayList<Posting>();
     public void addPosting(Posting posting) {
         // TODO
+        this.postingList.add(posting);
     }
 
     /**
      * Get the number of postings in the list
      */
     public int getDocFreq() {
-        // TODO
-        return 0;
+        // may be need to modify?
+        return this.postingList.size();
     }
 
     @Override
